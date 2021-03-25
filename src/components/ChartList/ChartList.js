4435584ad09b0b8ed0f1cbf9fb3b16916a6d22fe
Suzanna <hurstsuzanna@gmail.com>
2021-03-25 14:57:35 +0000

    added symbol list display via icon components

--- src/components/ChartList/ChartList.js
+++ src/components/ChartList/ChartList.js
@@ -3,37 +3,47 @@ import List from "@material-ui/core/List";
 // import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-
 import ThreeDRotation from "@material-ui/icons/ThreeDRotation";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//     maxWidth: 752,
-//   },
-//   demo: {
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   title: {
-//     margin: theme.spacing(4, 0, 2),
-//   },
-// }));
+import SignSymbol from "./SignSymbol";
+
+import { BirthChartContext } from "./../../contexts/birthchart.context";
 
 const ChartList = ({ data, ordered = false, ItemComponent }) => {
+  const { BirthChart } = useContext(BirthChartContext);
+
   return (
     <>
-      {Object.entries(data).map(([planet, sign]) => (
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <ThreeDRotation />
-            </ListItemIcon>
+      {Object.entries(data).map(([key, value]) => {
+        // console.log("key", key, "value", value);
+        if (BirthChart.planets.includes(key)) {
+          const { icon } = value;
+          const { sign } = value;
+          console.log("sign", sign);
+          console.log("icon", icon);
+          return (
             <li>
-              {planet} in {sign}
+              {key}: {sign}
+              {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="100 100 100 100">
+                {icon}
+              </svg> */}
+              <SignSymbol sign={sign} />
             </li>
-          </ListItem>
-        </List>
-      ))}
+          );
+        } else {
+          return null;
+        }
+      })}
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <ThreeDRotation />
+          </ListItemIcon>
+          {/* {console.log("planet", planet.sign)} */}
+          {/* <li>{planet} in </li> */}
+          {/* <SignSymbol {planet=planet}/> */}
+        </ListItem>
+      </List>
     </>
   );
 };
--- src/components/ChartList/SignSymbol.js
+++ src/components/ChartList/SignSymbol.js
@@ -1,12 +1,77 @@
-import React, { useContext } from "react";
-import { BirthChartContext } from "./../../contexts/birthchart.context";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-const SignSymbol = (chart) => {
-  const BirthChart = useContext(BirthChartContext);
-  const symbol = BirthChart.getIconSVG(chart[2]);
+import { ReactComponent as TaurusSymbol } from "./../../assets/symbols/taurus.svg";
+import { ReactComponent as AriesSymbol } from "./../../assets/symbols/aries.svg";
+import { ReactComponent as AquariusSymbol } from "./../../assets/symbols/aquarius.svg";
+import { ReactComponent as CancerSymbol } from "./../../assets/symbols/cancer.svg";
+import { ReactComponent as CapricornSymbol } from "./../../assets/symbols/capricorn.svg";
+import { ReactComponent as GeminiSymbol } from "./../../assets/symbols/gemini.svg";
+import { ReactComponent as LeoSymbol } from "./../../assets/symbols/leo.svg";
+import { ReactComponent as LibraSymbol } from "./../../assets/symbols/libra.svg";
+import { ReactComponent as PiscesSymbol } from "./../../assets/symbols/pisces.svg";
+import { ReactComponent as SagittariusSymbol } from "./../../assets/symbols/sagittarius.svg";
+import { ReactComponent as ScorpioSymbol } from "./../../assets/symbols/scorpio.svg";
+import { ReactComponent as VirgoSymbol } from "./../../assets/symbols/virgo.svg";
+// import { ReactComponent as Symbol } from `./../../assets/symbols/${sign}.svg`;
 
-  return;
-  <>{symbol}</>;
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    width: 40,
+  },
+}));
+
+const viewBox = "100 100 400 400";
+
+const SignSymbol = ({ sign }) => {
+  // console.log("icon from sign symbol", icon);
+  const classes = useStyles();
+
+  const getIconComponent = (sign) => {
+    switch (sign) {
+      case "Aries":
+        return <AriesSymbol className={classes.icon} />;
+        break;
+      case "Taurus":
+        return <TaurusSymbol className={classes.icon} />;
+        break;
+      case "Gemini":
+        return <GeminiSymbol className={classes.icon} />;
+        break;
+      case "Cancer":
+        return <CancerSymbol className={classes.icon} />;
+        break;
+      case "Leo":
+        return <LeoSymbol className={classes.icon} />;
+        break;
+      case "Virgo":
+        return <VirgoSymbol className={classes.icon} />;
+        break;
+      case "Libra":
+        return <LibraSymbol className={classes.icon} />;
+        break;
+      case "Scorpio":
+        return <ScorpioSymbol className={classes.icon} />;
+        break;
+      case "Sagittarius":
+        return <SagittariusSymbol className={classes.icon} />;
+        break;
+      case "Capricorn":
+        return <CapricornSymbol className={classes.icon} />;
+        break;
+      case "Aquarius":
+        return <AquariusSymbol className={classes.icon} />;
+        break;
+      case "Pisces":
+        return <PiscesSymbol className={classes.icon} />;
+        break;
+      default:
+        // console.log("default");
+        break;
+    }
+  };
+
+  return <>{getIconComponent(sign)}</>;
 };
 
 export default SignSymbol;
